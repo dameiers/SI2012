e41a4e2ff4c3612aa247dd922f596d41551bb53e
dameiers <daniel.meiers@cismet.de>
2012-08-14 14:04:19 +0200

    the interface.... now really...

---
+++ src/gui/steps/AbstractViewModelConnectionImpl.java
@@ -0,0 +1,17 @@
+package gui.steps;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import model.steps.InformationGatherStepModel;
+
+public abstract class AbstractViewModelConnectionImpl extends JPanel implements ViewModelConnection{
+
+
+	@Override
+	public JComponent getVisualisationUI() {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+}
